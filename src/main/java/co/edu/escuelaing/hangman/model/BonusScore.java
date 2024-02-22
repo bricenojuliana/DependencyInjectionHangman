2ec7d11f6480ee4408766eee5ae59959e4a1d211
Cristian <cristian.naranjo@mail.escuelaing.edu.co>
2024-02-22 18:22:09 -0500

    implementacion del modelo

--- src/main/java/co/edu/escuelaing/hangman/model/BonusScore.java
+++ src/main/java/co/edu/escuelaing/hangman/model/BonusScore.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component("bonusScore")
 public class BonusScore implements GameScore{
+
+
     public BonusScore() {
     }
 
@@ -17,7 +19,14 @@ public class BonusScore implements GameScore{
      * y penalizacion de 5 puntos por cada letra incorrecta.
      */
     @Override
-    public int calculateScore(int correctCount, int incorrectCount) {
-        return 0;
+    public int calculateScore(int correctCount, int incorrectCount) throws IllegalArgumentException {
+        if (correctCount < 0 || incorrectCount < 0){
+            throw new IllegalArgumentException("Ningun contador puede ser negativo.");
+        }
+        int score = 0;
+        score += correctCount*10;
+        score -= incorrectCount*5;
+        return score <= 0 ? 0 : score;
     }
+
 }
